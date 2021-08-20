06100e7e2c95fa722a3de50473408d0eb8b8bb85
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 11:16:11 +0530

    3rd edition

--- JavaDemo/src/com/bridgelabz/demo/JavaDemo.java
+++ JavaDemo/src/com/bridgelabz/demo/JavaDemo.java
@@ -4,5 +4,6 @@ public class JavaDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello git and Java bros!");
 		System.out.println(" new feature is added in the uc1");
+		System.out.println(" new feature is added in the uc2");
 	}
 }
