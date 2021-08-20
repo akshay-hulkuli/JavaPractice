ede8a7ba8cca826ffaea694fe9bb7b2d5d842c06
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-20 11:11:46 +0530

    the second edition

--- JavaDemo/src/com/bridgelabz/demo/JavaDemo.java
+++ JavaDemo/src/com/bridgelabz/demo/JavaDemo.java
@@ -3,5 +3,6 @@ package com.bridgelabz.demo;
 public class JavaDemo {
 	public static void main(String[] args) {
 		System.out.println("Hello git and Java bros!");
+		System.out.println(" new feature is added in the uc1");
 	}
 }
